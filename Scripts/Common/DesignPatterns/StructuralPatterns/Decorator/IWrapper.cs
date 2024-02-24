058745ec6d69d12159d1767b594cdaecd9ab0be6
ab5ent <inf.ng.nam@proton.me>
2024-02-24 10:15:57 +0700

    Decorator

---
+++ Scripts/Common/DesignPatterns/StructuralPatterns/Decorator/IWrapper.cs
@@ -0,0 +1,10 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace ab5entSDK.Common.DesignPatterns.StructuralPatterns.Decorator
+{
+    public interface IWrapper
+    {
+    }
+}
