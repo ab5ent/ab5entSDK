2255b9c21e4d27372ee40a3a10d020fc6f6304da
ab5ent <156503250+ab5ent@users.noreply.github.com>
2024-11-11 21:53:41 +0700

    ManagerSystem

--- Scripts/Common/Systems/ManagerSystem/RootManager.cs
+++ Scripts/Common/Systems/ManagerSystem/RootManager.cs
@@ -1,45 +1,21 @@
-using System;
-using System.Collections.Generic;
 using UnityEngine;
 
 namespace ab5entSDK.Common.Systems.ManagerSystem
 {
+    [RequireComponent(typeof(ListManagers))]
     public class RootManager : MonoBehaviour
     {
-        protected Dictionary<Type, Manager> managers;
+        protected ListManagers listManagers;
 
         protected virtual void Awake()
         {
-            Initialize();
-        }
-
-        private void Initialize()
-        {
-            managers = new Dictionary<Type, Manager>();
-
-            Manager[] allManagers = GetComponentsInChildren<Manager>(true);
-
-            for (int i = 0; i < allManagers.Length; i++)
-            {
-                allManagers[i].SetRoot(this);
-                managers.Add(allManagers[i].GetType(), allManagers[i]);
-            }
-
-            for (int i = 0; i < allManagers.Length; i++)
-            {
-                allManagers[i].Initialize();
-            }
+            listManagers = GetComponent<ListManagers>();
+            listManagers.Initialize(this);
         }
 
         public virtual T GetManager<T>() where T : Manager
         {
-            if (managers.TryGetValue(typeof(T), out Manager component))
-            {
-                return component as T;
-            }
-
-            return null;
+            return listManagers.GetManager<T>();
         }
-
     }
 }
